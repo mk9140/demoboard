2ae7685b34acf80dd68004f0d14ed791eb67cf87
mk9140 <31506088+mk9140@users.noreply.github.com>
2021-03-09 14:12:11 +0900

    Initial commit

---
+++ src/main/java/com/boardex/demo/DemoboardApplication.java
@@ -0,0 +1,13 @@
+package com.boardex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardex/demo/DemoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardex.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
